31b9e5587b36a503ffc445347b33e7fcf882e840
SevvalKolaca <sevval.kolaca.00@gmail.com>
2025-01-21 18:40:43 +0300

    Kategori işlevleri oluşturuldu. (alt kategoriler incelenecek)

--- src/main/java/org/turkcell/ecommercepair5/dto/category/DeleteCategoryDto.java
+++ src/main/java/org/turkcell/ecommercepair5/dto/category/DeleteCategoryDto.java
@@ -1,4 +1,22 @@
 package org.turkcell.ecommercepair5.dto.category;
 
 public class DeleteCategoryDto {
+    private Integer id;
+    private Boolean isActive;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
 }
--- src/main/java/org/turkcell/ecommercepair5/service/SubcategoryService.java
+++ src/main/java/org/turkcell/ecommercepair5/service/SubcategoryService.java
@@ -1,11 +1,23 @@
 package org.turkcell.ecommercepair5.service;
 
+import org.turkcell.ecommercepair5.dto.subcategory.CreateSubcategoryDto;
+import org.turkcell.ecommercepair5.dto.subcategory.DeleteSubcategoryDto;
+import org.turkcell.ecommercepair5.dto.subcategory.SubcategoryListingDto;
 import org.turkcell.ecommercepair5.entity.Category;
 import org.turkcell.ecommercepair5.entity.Subcategory;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SubcategoryService {
 
     Optional<Subcategory> findById(Integer id);
+
+    void add(CreateSubcategoryDto createSubcategoryDto);
+
+    void delete(DeleteSubcategoryDto deleteSubcategoryDto);
+
+    List<SubcategoryListingDto> getAll();
+
+    List<SubcategoryListingDto> listingSubcategoriesByCategoryId(Integer categoryId);
 }
